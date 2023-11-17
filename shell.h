5a14c6acca94137e4efbc0b7675ddc1d33bf5e7e
ItumelengZulu <itumelengzulu@gmail.com>
2023-11-17 13:34:24 +0200

    0x16. C - Simple Shell

--- shell.h
+++ shell.h
@@ -20,6 +20,7 @@ extern char **environ;
 void display_prompt(void);
 char *read_command(void);
 void execute_command(char *command);
+char *trim_spaces(char *str);
 pid_t create_child_process(void);
 pid_t get_process_id(void);
 pid_t get_parent_process_id(void);
